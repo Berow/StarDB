d6292fa1c1021b745e92ad5a803f0f437cde904c
Berow <52027524+Berow@users.noreply.github.com>
2019-12-22 18:28:02 +0300

    Add files via upload

---
+++ precache-manifest.b3f13ec5a66edb07136ee00f370e9605.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bdc13e6f53399d20648e1bb44e004f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20d4ffdcb066221f7f55",
+    "url": "/static/css/main.aaddd09c.chunk.css"
+  },
+  {
+    "revision": "f23ac108b0dc0674f4c7",
+    "url": "/static/js/2.e7d6d1b7.chunk.js"
+  },
+  {
+    "revision": "20d4ffdcb066221f7f55",
+    "url": "/static/js/main.40a08360.chunk.js"
+  },
+  {
+    "revision": "84f4725beaac68d6a741",
+    "url": "/static/js/runtime-main.f8a02862.js"
+  },
+  {
+    "revision": "35adb407793ba65eff78b70510314782",
+    "url": "/static/media/loading.35adb407.svg"
+  }
+]);
